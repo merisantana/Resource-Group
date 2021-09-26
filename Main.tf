4467b8e642db1df5ede80b1c4a0503e727fb510c
merisantana <86296843+merisantana@users.noreply.github.com>
2021-09-26 21:20:00 +0200

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,4 +1,10 @@
-resource "azurerm_resource_group" "RG" {
-  name     = var.name_Resource_Group
+provider "azurerm" {
+
+  version = "=2.20.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = var.name_resource_group
   location = var.location
 }
